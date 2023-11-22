308f7f7b6ff307e2ba8fb366751021b43cc37cdd
beavelar <beavelar.github@gmail.com>
2023-11-21 22:10:26 -0700

    Update config

--- lua/personal/set.lua
+++ lua/personal/set.lua
@@ -19,10 +19,12 @@ vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
