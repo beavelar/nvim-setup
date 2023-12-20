c584603898c221c9a0fcb6bf118d05482c57721e
beavelar <beavelar.github@gmail.com>
2023-12-20 15:37:04 -0700

    Set keymap for close and toggle

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -1,3 +1,5 @@
 local lib = require("diffview")
 
-vim.keymap.set("n", "<leader>di", lib.open)
+vim.keymap.set("n", "<leader>do", lib.open)
+vim.keymap.set("n", "<leader>dq", lib.close)
+vim.keymap.set("n", "<leader>dt", function() lib.emit("toggle_files") end)
